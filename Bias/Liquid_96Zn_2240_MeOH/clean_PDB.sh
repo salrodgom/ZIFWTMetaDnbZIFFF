fda0e0305b770c15fe30f4c6a9c82de8eaea75b3
Salvador R.G. Balestra <S.Rodriguez.Gomez@csic.es>
2022-06-28 03:12:25 +0200

    modified:   Bias/Liquid_96Zn_2240_MeOH/clean_PDB.sh 	modified:   Bias/Liquid_96Zn_2240_MeOH/in.lmp 	modified:   Bias/Liquid_96Zn_2240_MeOH/index.ndx 	modified:   Bias/Liquid_96Zn_2240_MeOH/optimised.data 	modified:   Bias/Liquid_96Zn_2240_MeOH/p1_fromPDFFile.data 	modified:   Bias/Liquid_96Zn_2240_MeOH/plumed.txt

--- Bias/Liquid_96Zn_2240_MeOH/clean_PDB.sh
+++ Bias/Liquid_96Zn_2240_MeOH/clean_PDB.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 timestep=$1
 if [ ! -z "$timestep" ] ; then echo "TimeStep=$timestep" ; else timestep=0.0 ; fi
-echo "0 " | gmx traj -f MDNVT.xtc -s p1_fromPDFFile.pdb -oxt traj.pdb -b $timestep -tu ps -nojump yes
+echo "15 " | gmx_mpi traj -f MDNVT.xtc -s p1_fromPDFFile.pdb -oxt traj.pdb -b $timestep -tu ps -nojump yes -n index.ndx
 rm -rf \#*
 exit 0
